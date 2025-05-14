fde7cb299a6fba0bd0bc631ccd1e60cf1cfb405c
Rakshit <140930768+Rakshitpotti@users.noreply.github.com>
2025-05-14 20:40:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,4 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-
-
-// authconfig
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import { MsalProvider } from "@azure/msal-react";
-// import { PublicClientApplication } from "@azure/msal-browser";
-// import { msalConfig } from "./authConfig";
-
-// const msalInstance = new PublicClientApplication(msalConfig);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <MsalProvider instance={msalInstance}>
-//       <App />
-//     </MsalProvider>
-//   </React.StrictMode>
-// );
-
-
-
-
-// index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
